dc5f478d098af9a4bb01c9a5312a95d7c901b131
SANG HAN <rv498@hotmail.com>
2017-11-20 15:49:28 -0800

    1st COmmit

---
+++ app.js
@@ -0,0 +1,28 @@
+(function () {
+	
+	'use strict';
+	
+	angular.module('app1', [])
+	.controller('controller1', function($scope) {
+		
+		$scope.content1 = "";
+		$scope.QuantityScale = "";
+		$scope.clickedFunc = function () {
+			
+			var splitedContent = $scope.content1.split(",");
+			if (splitedContent.length <= 3) {
+				$scope.QuantityScale = "Enjoy!";
+			} else if (splitedContent.length > 4 ) {
+				$scope.QuantityScale = "Too Much!";
+			} else
+				{
+					$scope.QuantityScale = "Please enter data first";
+				}
+			
+		};
+		
+	});
+	
+	
+	
+})();
