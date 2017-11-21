95f97b7eace2957b41f3277566109358da9d3fc0
SANG HAN <rv498@hotmail.com>
2017-11-21 06:33:55 -0800

    4th commit

--- app.js
+++ app.js
@@ -10,14 +10,14 @@
 		$scope.clickedFunc = function () {
 			
 			var splitedContent = $scope.content1.split(",");
-			
-			  if (splitedContent.length <= 3 && splitedContent.length > 0) {
+			if (splitedContent == "") {
+				$scope.QuantityScale = "Please enter data first";
+			}
+			  else if (splitedContent.length <= 3 && splitedContent.length > 0) {
 				$scope.QuantityScale = "Enjoy!";
 			} else if (splitedContent.length > 3 ) {
 				$scope.QuantityScale = "Too Much!";
-			}  else {
-				$scope.QuantityScale = "Please enter data first";
-			}
+			}   
 		};
 		
 	});
