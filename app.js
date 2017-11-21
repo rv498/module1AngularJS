92867ad31c3d5f48a46bc3cd8bf56f0045ccb673
SANG HAN <rv498@hotmail.com>
2017-11-20 17:43:22 -0800

    3rd Commit

--- app.js
+++ app.js
@@ -5,20 +5,19 @@
 	angular.module('app1', [])
 	.controller('controller1', function($scope) {
 		
-	//	$scope.content1 = "";
-		$scope.QuantityScale = "Please enter data first";
+ 
+		
 		$scope.clickedFunc = function () {
 			
 			var splitedContent = $scope.content1.split(",");
-			if (splitedContent.length <= 3) {
-				$scope.QuantityScale = "Enjoy!";
-			} else if (splitedContent.length > 4 ) {
-				$scope.QuantityScale = "Too Much!";
-			} else
-				{
-					$scope.QuantityScale = "Please enter data first";
-				}
 			
+			  if (splitedContent.length <= 3 && splitedContent.length > 0) {
+				$scope.QuantityScale = "Enjoy!";
+			} else if (splitedContent.length > 3 ) {
+				$scope.QuantityScale = "Too Much!";
+			}  else {
+				$scope.QuantityScale = "Please enter data first";
+			}
 		};
 		
 	});
